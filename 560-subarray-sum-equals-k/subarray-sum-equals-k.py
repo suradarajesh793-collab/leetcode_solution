08b148ad5816d72a1bc100243c7eb861620462f2
Rajesh_7 <suradarajesh793@gmail.com>
2026-09-09 11:51:06 +0530

    Time: 27 ms (90.94%) | Memory: 21.8 MB (56.43%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,15 +1,12 @@
 class Solution:
     def subarraySum(self, nums, k):
-        count = 0
-        prefix = 0
-        freq = {0: 1}
-
-        for num in nums:
-            prefix += num
-
-            if prefix - k in freq:
-                count += freq[prefix - k]
-
-            freq[prefix] = freq.get(prefix, 0) + 1
-
-        return count
+        count={0:1}
+        prefix=0
+        ans=0
+        for x in nums:
+            prefix+=x
+            su=prefix-k
+            if su in count:
+                ans+=count[su]
+            count[prefix]=count.get(prefix,0)+1
+        return ans

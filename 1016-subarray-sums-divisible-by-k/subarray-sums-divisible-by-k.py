478264005c908262c91ff619db16e142ca05c701
Rajesh_7 <suradarajesh793@gmail.com>
2026-09-18 07:32:15 +0530

    Time: 27 ms (87.09%) | Memory: 23.4 MB (86.42%) - LeetSync

--- 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.py
+++ 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.py
@@ -4,11 +4,11 @@ class Solution:
         prefix=0
         ans=0
         for x in nums:
-            prefix +=x
+            prefix+=x
             rem=prefix%k
             if rem in count:
                 ans+=count[rem]
-            count[rem]=count.get(rem,0) + 1
+            count[rem]=count.get(rem,0)+1
         return ans 
 
         
